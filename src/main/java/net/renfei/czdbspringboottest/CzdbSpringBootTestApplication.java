09a2c19914499d6581d80c456cc792f575c42152
RenFei <i@renfei.net>
2024-08-08 16:07:10 +0800

    init repo

---
+++ src/main/java/net/renfei/czdbspringboottest/CzdbSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package net.renfei.czdbspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzdbSpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzdbSpringBootTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/renfei/czdbspringboottest/CzdbSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.renfei.czdbspringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzdbSpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
